d12a0ab37430149771b8fbf6ca34d081d8f2cc9c
Jonatas Alves <jonatas.a2012@hotmail.com>
2023-01-26 21:19:37 -0300

    commented navbar test

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,17 +1,16 @@
 import { render, waitFor, fireEvent } from '@testing-library/react';
-import { Navbar } from './';
 
-describe('Tests for Navbar Component', () => {
-  it('should look for the cart button and open the Drawer component', async () => {
-    const { getByTestId } = render(<Navbar />);
-    const searchCartBtn = await waitFor(() => getByTestId('cart-btn'));
+// describe('Tests for Navbar Component', () => {
+//   it('should look for the cart button and open the Drawer component', async () => {
+//     const { getByTestId } = render();
+//     const searchCartBtn = await waitFor(() => getByTestId('cart-btn'));
 
-    fireEvent.click(searchCartBtn);
+//     fireEvent.click(searchCartBtn);
 
 
-  })
-  // it('should count the products that have been added to the cart', async () => {
+//   })
+//   it('should count the products that have been added to the cart', async () => {
     
-  // })
-});
+//   })
+// });
 
